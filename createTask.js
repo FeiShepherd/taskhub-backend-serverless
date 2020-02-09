f8c239bd395c95379c90ea32e524e86fcf0cc4ff
FeiShepherd <thomasshepherd2002@gmail.com>
2020-02-09 16:55:01 +0000

    Update create to es6

--- createTask.js
+++ createTask.js
@@ -3,45 +3,43 @@ import uuid from "uuid";
 import AWS from "aws-sdk";
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Credentials": true
+};
 
-export const main = (event, context, cb) => {
-  const data = JSON.parse(event.body);
+export const main = async (event, context) => {
+  const {title, content, startDate, endDate, attachment} = JSON.parse(event.body)
 
   const params = {
     TableName: process.env.tableName,
     Item: {
+      title,
+      content,
+      startDate,
+      endDate,
+      attachment,
       userId: event.requestContext.identity.cognitoIdentityId,
       taskId: uuid.v1(),
-      title: data.title,
-      content: data.content,
-      attachment: data.attachment,
       createdAt: Date.now(),
-      startDate: Date.now(),
-      endDate: Date.now()
     }
   };
 
-  dynamoDb.put(params, (error, data) => {
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
-
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false })
-      };
-      cb(null, response);
-      return;
-    }
-
-    const response = {
+  try{
+    const dbData =await dynamoDb.put(params)
+    
+    return {
       statusCode: 200,
       headers: headers,
       body: JSON.stringify(params.Item)
     };
-    cb(null, response);
-  })
+  }
+  catch(error){
+    
+    return {
+      statusCode: 500,
+      headers: headers,
+      body: JSON.stringify({ status: false, error})
+    };
+  }
 };
